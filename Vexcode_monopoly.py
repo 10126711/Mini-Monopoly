ceee925534ebf22e008bac889d35b4e723ef6538
Rion Shani <10126711@philasd.org>
2026-09-15 18:25:45 +0000

    commit 3

--- Vexcode_monopoly.py
+++ Vexcode_monopoly.py
@@ -15,32 +15,14 @@ def play_game():
         roll_dice()
         move()
         complete_task()
+        brain.screen.clear_row(4)
+        brain.screen.set_cursor(brain.screen.row(), 1)
+        brain.screen.set_cursor(1, 1)
         wait(5, MSEC)
-
-def move():
-    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
-    print("VEXcode", end="")
-    dice1 = int(round(urandom.uniform(1, 4), 2))
-    brain.screen.print(str("Rolled a:") + str(dice1))
-    brain.screen.next_row()
-    dice2 = int(round(urandom.uniform(1, 4), 2))
-    brain.screen.print(str("Rolled a:") + str(dice2))
-    brain.screen.next_row()
-    spaces_to_move = dice1 + dice2
-
-def roll_dice():
-    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
-    dice1 = int(round(urandom.uniform(1, 4), 2))
-    brain.screen.print(str("Rolled a:") + str(dice1))
-    brain.screen.next_row()
-    dice2 = int(round(urandom.uniform(1, 4), 2))
-    brain.screen.print(str("Rolled a:") + str(dice2))
-    brain.screen.next_row()
-    spaces_to_move = dice1 + dice2
 
 def complete_task():
     global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
-    brain.screen.print(str("Landed on space") + str(curren_space_numbers))
+    brain.screen.print(str("Landed on ") + str(curren_space_numbers))
     brain.screen.next_row()
     wait(1, SECONDS)
     if curren_space_numbers == 1:
@@ -60,7 +42,7 @@ def complete_task():
         pass
     if curren_space_numbers == 1:
         # Space = GO
-        for repeat_count2 in range(4):
+        for repeat_count in range(4):
             left_turn.broadcast_and_wait()
             wait(5, MSEC)
     elif curren_space_numbers == 4:
@@ -69,17 +51,17 @@ def complete_task():
     elif curren_space_numbers == 7:
         # Space = Free Parking
         wait(5, SECONDS)
-    elif curren_space_numbers == 7:
+    elif curren_space_numbers == 10:
         # Space = Go to Jail
         right_turn.broadcast_and_wait()
-        for repeat_count3 in range(3):
+        for repeat_count2 in range(3):
             move_forward.broadcast_and_wait()
             wait(5, MSEC)
         left_turn.broadcast_and_wait()
-        for repeat_count4 in range(3):
+        for repeat_count3 in range(3):
             move_forward.broadcast_and_wait()
             wait(5, MSEC)
-        for repeat_count5 in range(2):
+        for repeat_count4 in range(2):
             right_turn.broadcast_and_wait()
             wait(5, MSEC)
         curren_space_numbers = 4
@@ -88,16 +70,78 @@ def complete_task():
         # Space + Blue 1 or 2, Green 1 or 2, Yellow 1 or 2, or Red 1 or 2
         right_turn.broadcast_and_wait()
         move_forward.broadcast_and_wait()
-        for repeat_count6 in range(2):
+        for repeat_count5 in range(2):
             left_turn.broadcast_and_wait()
             wait(5, MSEC)
         move_forward.broadcast_and_wait()
         right_turn.broadcast_and_wait()
 
+def move():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    brain.screen.print(str("Moving") + str(str(spaces_to_move) + str("spaces!")))
+    brain.screen.next_row()
+    for repeat_count6 in range(int(spaces_to_move)):
+        move_forward.broadcast_and_wait()
+        curren_space_numbers = curren_space_numbers + 1
+        if curren_space_numbers > 12:
+            curren_space_numbers = 1
+        if curren_space_numbers == 1:
+            right_turn.broadcast_and_wait()
+        if curren_space_numbers == 4:
+            right_turn.broadcast_and_wait()
+        if curren_space_numbers == 7:
+            right_turn.broadcast_and_wait()
+        if curren_space_numbers == 10:
+            right_turn.broadcast_and_wait()
+        wait(5, MSEC)
+
+def roll_dice():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    dice1 = int(round(urandom.uniform(1, 4), 2))
+    brain.screen.print(str("Rolled a:") + str(dice1))
+    brain.screen.next_row()
+    dice2 = int(round(urandom.uniform(1, 4), 2))
+    brain.screen.print(str("Rolled a:") + str(dice2))
+    brain.screen.next_row()
+    spaces_to_move = dice1 + dice2
+
 def when_started1():
     global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
-    spaces_to_move = 1
+    curren_space_numbers = 1
     play_game()
 
-when_started1()
+def move_forward_callback_0():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    motor_1.spin_for(FORWARD, 400, DEGREES)
 
+def move_forward_callback_1():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    motor_5.spin_for(FORWARD, 400, DEGREES)
+
+def left_turn_callback_0():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    motor_1.spin_for(REVERSE, 215, DEGREES)
+
+def left_turn_callback_1():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    motor_5.spin_for(FORWARD, 215, DEGREES)
+
+def right_turn_callback_0():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    motor_5.spin_for(REVERSE, 215, DEGREES)
+
+def right_turn_callback_1():
+    global myVariable, spaces_to_move, dice1, dice2, curren_space_numbers, move_forward, my_event, right_turn, left_turn, screen_precision, console_precision
+    motor_1.spin_for(FORWARD, 215, DEGREES)
+
+# system event handlers
+move_forward(move_forward_callback_0)
+move_forward(move_forward_callback_1)
+left_turn(left_turn_callback_0)
+left_turn(left_turn_callback_1)
+right_turn(right_turn_callback_0)
+right_turn(right_turn_callback_1)
+# add 15ms delay to make sure events are registered correctly.
+wait(15, MSEC)
+
+when_started1()
